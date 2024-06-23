9fcae224af2ed519f74948ef194c07b5cc132b16
gurselturkeri <gurselt33@gmail.com>
2024-06-23 11:29:22 +0300

    change gif in readme

--- launch/robot.launch.py
+++ launch/robot.launch.py
@@ -1,12 +1,15 @@
 import os
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, ExecuteProcess
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
     urdf_file_path = '/home/gursel/ros2_ws/src/diff_robot/urdf/diff_robot.urdf'
     rviz_config_file_path = '/home/gursel/ros2_ws/src/diff_robot/rviz/urdf_config.rviz'
+    slam_params_file_path = '/home/gursel/ros2_ws/src/diff_robot/config/slam_params.yaml'
 
     with open(urdf_file_path, 'r') as infp:
         robot_desc = infp.read()
@@ -36,5 +39,20 @@ def generate_launch_description():
         Node(package='rviz2', executable='rviz2',
              name='rviz2',
              output='screen',
-             arguments=['-d', LaunchConfiguration('rvizconfig')])
+             arguments=['-d', LaunchConfiguration('rvizconfig')]),
+
+        Node(package='slam_toolbox', executable='sync_slam_toolbox_node',
+             name='slam_toolbox',
+             output='screen',
+             parameters=[{'use_sim_time': True, 'slam_params_file': slam_params_file_path}]),
+
+        Node(package='tf2_ros', executable='static_transform_publisher',
+             name='static_transform_publisher_lidar',
+             output='screen',
+             arguments=['0', '0', '0', '0', '0', '0', 'base_link', 'lidar_link']),
+
+        Node(package='tf2_ros', executable='static_transform_publisher',
+             name='static_transform_publisher_imu',
+             output='screen',
+             arguments=['0', '0', '0', '0', '0', '0', 'base_link', 'imu_link'])
     ])
